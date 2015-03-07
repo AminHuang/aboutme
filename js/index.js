a99219dd635fef64298e1f993e414879f15cc756
AminHuang <skuare520@gmail.com>
2015-03-07 19:59:23 +0800

    搭好框架

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    setMyCanvas ();
+
+});
+
+// 不能自适应屏幕
+function setMyCanvas () {
+    var red = [0, 100, 63];
+    var orange = [40, 100, 60];
+    var green = [75, 100, 40];
+    var blue = [196, 77, 55];
+    var purple = [280, 50, 60];
+
+    var myName = "AminHuang";
+    var letterColors=[red,orange,green];
+    if(10 < 3){
+        bubbleShape = "circle";
+    } else {
+        bubble = "square";
+    }
+
+
+    drawName(myName, letterColors);
+    bounceBubbles();
+}
